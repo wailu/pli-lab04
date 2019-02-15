5aaeb8d65b429bc2f021a46073e462643f5c41fb
wailunlim <wailun@u.nus.edu>
2019-02-15 20:21:37 +0800

    Complete EplBigStepSemantics

--- src/main/scala/edu/nus/comp/pli/epl/bigstepsemantics/EplBigStepSemantics.scala
+++ src/main/scala/edu/nus/comp/pli/epl/bigstepsemantics/EplBigStepSemantics.scala
@@ -1,6 +1,7 @@
 package edu.nus.comp.pli.epl.bigstepsemantics
 
 import edu.nus.comp.pli.epl.parser.EplAST.{Minus, _}
+import javax.naming.spi.DirStateFactory.Result
 
 object EplBigStepSemantics {
 
@@ -26,27 +27,35 @@ object EplBigStepSemantics {
     }
     case Bin(Sub, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultInt(valueLeft - valueRight)
       case _ => EvaluationError
     }
     case Bin(Mul, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultInt(valueLeft * valueRight)
       case _ => EvaluationError
     }
     case Bin(Div, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultInt(valueLeft / valueRight)
       case _ => EvaluationError
     }
 
     case Bin(Pow, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultInt(math.pow(valueLeft, valueRight).toInt)
       case _ => EvaluationError
     }
     case Bin(Eq, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultBool(valueLeft), ResultBool(valueRight)) => ResultBool(valueLeft == valueRight)
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultBool(valueLeft == valueRight)
       case _ => EvaluationError
     }
     case Bin(NEq, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultBool(valueLeft), ResultBool(valueRight)) => ResultBool(valueLeft != valueRight)
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultBool(valueLeft != valueRight)
       case _ => EvaluationError
     }
     case Bin(Lt, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
@@ -55,26 +64,33 @@ object EplBigStepSemantics {
     }
     case Bin(Lte, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultBool(valueLeft <= valueRight)
       case _ => EvaluationError
     }
     case Bin(Gt, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultBool(valueLeft > valueRight)
       case _ => EvaluationError
     }
     case Bin(Gte, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultInt(valueLeft), ResultInt(valueRight)) => ResultBool(valueLeft >= valueRight)
       case _ => EvaluationError
     }
     case Bin(And, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultBool(valueLeft), ResultBool(valueRight)) => ResultBool(valueLeft && valueRight)
       case _ => EvaluationError
     }
     case Bin(Or, exprLeft, exprRight) => (eval(exprLeft), eval(exprRight)) match {
       //add your code
+      case (ResultBool(valueLeft), ResultBool(valueRight)) => ResultBool(valueLeft || valueRight)
       case _ => EvaluationError
     }
     case IfElse(cond, ifbranch, elsebranch) => (eval(cond),eval(ifbranch),eval(elsebranch)) match {
       //add your code
+      case (ResultBool(true), ifValue, elseValue) => ifValue
+      case (ResultBool(false), ifValue, elseValue) => elseValue
       case _ => EvaluationError
     }
 
